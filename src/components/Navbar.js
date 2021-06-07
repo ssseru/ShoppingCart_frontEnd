84d168e791717fb4eb3116966246280f922a798d
Sai Sagar Seru <saisseru@gmail.com>
2021-06-07 23:01:09 +0530

    Working Cart

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper" style={{ backgroundColor: "#3e89d5" }}>
+      <div className="container">
+        <Link to="/" className="brand-logo">
+          Store
+        </Link>
+
+        <ul className="right">
+          <li>
+            <Link to="/">Catalog</Link>
+          </li>
+          <li>
+            <Link to="/cart">My Cart</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="material-icons">shopping_cart</i>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
